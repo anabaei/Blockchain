4321c71fa22b797851d9811e79cedea7ba363830
Amir Nabaei <nabaei17@gmail.com>
2018-07-31 22:42:10 -0700

    add demo and hello world functions

---
+++ demo/helloworld/contracts/helloworld.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.4.4;
+contract HelloWorld
+{
+    function SayHello() public returns (string)
+    {
+        return("Hello World");
+    }
+}
---
+++ demo/helloworld/migrations/1_initial_migration.js
@@ -0,0 +1,8 @@
+var Migrations = artifacts.require("./Migrations.sol");
+
+var HelloWorld = artifacts.require("./helloworld.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(Migrations);
+  deployer.deploy(HelloWorld);
+};

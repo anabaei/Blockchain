8663a11269419bc023323a0b610ea75fd2211cc6
Amir Nabaei <nabaei17@gmail.com>
2018-08-01 16:10:56 -0700

    add user interface app foodsafe

---
+++ demo/foodsafe/contracts/FoodSafe.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.4.17;
+
+contract FoodSafe
+{
+ struct Location{
+     string Name;
+     uint LocationId;
+     uint PreviousLocationId;
+     uint Timestamp;
+     string Secret;
+ }
+ mapping(uint => Location) Trail;
+ uint8 TrailCount=0;
+
+ function AddNewLocation(uint LocationId, string Name, string Secret)
+ {
+    Location memory newLocation;
+    newLocation.Name = Name;
+    newLocation.LocationId = LocationId;
+    newLocation.Secret = Secret; 
+    newLocation.Timestamp = now;
+   if(TrailCount!=0)
+   {
+       newLocation.PreviousLocationId = Trail[TrailCount].LocationId;
+   }
+   Trail[TrailCount] = newLocation;
+   TrailCount++;
+ }   
+   function GetTrailCount() returns(uint8) {
+       return TrailCount;
+   }
+   
+   function GetLocation
+
+}
